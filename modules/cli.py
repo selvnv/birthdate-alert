685c60f75d013b703628692e894e40b11200424d
selvnv <selvnv@post-inform.ru>
2026-04-29 21:23:02 +0700

    Implement add command

--- modules/cli.py
+++ modules/cli.py
@@ -16,15 +16,25 @@ def init(file):
 
 
 @cli.command(name="add")
-def add():
-    pass
+@click.option("--name", "-n", type=str, required=True)
+@click.option("--birthdate", "-b", type=click.DateTime(formats=["%Y-%m-%d"]), required=True)
+@click.option("--additional", "-a", type=str, required=False, default=None)
+def add(name, birthdate, additional):
+    db = Database("birthday_info", "db", "birthday_info")
+    try:
+        db.add(name, birthdate, additional)
+    except Exception as error:
+        print(f"\033[1m\033[93m[ERROR] >>>>\033[0m Error while add", error)
 
 
 @cli.command(name="list")
 def lst():
     db = Database("birthday_info", "db", "birthday_info")
-    result = db.fetch_all()
-    print(result)
+    try:
+        result = db.fetch_all()
+        print(result)
+    except Exception as error:
+        print(f"\033[1m\033[93m[ERROR] >>>>\033[0m Error while list", error)
 
 
 @cli.command(name="delete")
